93426f4cf302f4796065b99371d063161c2e429d
Omdarade1399 <150209165+Omdarade1399@users.noreply.github.com>
2024-10-03 17:27:25 +0530

    made a simple counter to learn useState hook

--- my-new-app/src/App.jsx
+++ my-new-app/src/App.jsx
@@ -1,10 +1,34 @@
-
+import { useState } from 'react';
+import './App.css'
 
 function App() {
+
+  const [counter, setCounter] = useState(0)
   
+  const addValue = () => {
+    if(counter >= 0 && counter <= 19){
+    setCounter(counter + 1)
+    }
+  }
+
+  const removeValue = () => {
+    if(counter <= 20 && counter >= 1){
+    setCounter(counter - 1)
+    }
+  }
 
   return (
-    <h1>Yo</h1>
+    <>
+      <h1>My New App</h1>
+      <h2>Counter value: {counter}</h2>
+      <button
+      onClick={addValue}
+      >Add Value</button>
+      <br />
+      <button
+      onClick={removeValue}
+      >Remove Value</button>
+    </>
   )
 }
 
