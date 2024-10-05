2b8b35e07c3932caef532b4db3fff9029f1b8e00
Omdarade1399 <150209165+Omdarade1399@users.noreply.github.com>
2024-10-05 15:54:47 +0530

    Built a Password Generator

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
 import './App.css'
 
 function App() {
@@ -7,6 +7,8 @@ function App() {
   const [allowChar, setAllowChar] = useState(false);
   const [password, setPassword] = useState("");
 
+  const passwordRef = useRef(null)
+
   const passwordGenerator = useCallback(() => {
     let pass = ""
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
@@ -14,25 +16,35 @@ function App() {
     if (allowNum) str += "0123456789"
     if (allowChar) str += "!@#$^&*-_+=[]{}~`"
 
-    for (let i = 1; i <= array.length; i++) {
+    for (let i = 1; i <= length; i++) {
       const char = Math.floor(Math.random() * str.length + 1);
-      pass = str.charAt(char)
+      pass += str.charAt(char)
       }
       setPassword(pass)
 
   }, [length, allowNum, allowChar, setPassword])
 
+  const copyToClipBoard = useCallback(() => {
+    passwordRef.current?.select()
+    window.navigator.clipboard.writeText(password)
+    alert(`Copied to Clipboard: "${password}"`)
+  }, [password])
+
+  useEffect(() => {
+    passwordGenerator()
+  }, [length, allowNum, allowChar, passwordGenerator])
+
   return (
     <>
      <div className="w-full max-w-lg mx-auto shadow-md rounded-lg px-4 py-3 my-60 text-orange-500 bg-gray-800">
-     <h1 className='text-3xl text-center text-white my-3'>Password Generator</h1>
-     <div className="flex shadow rounded-lg overflow-hidden mb-4">
-      <input type="text" value={password} className='outline-none w-full py-1 px-3' placeholder="Password" readOnly/>
-      <button className='outline-none bg-blue-700 text-white px-3 py-o.5 shrink-0'>Copy</button>
+     <h1 className='text-3xl text-center text-white mb-3'>Password Generator</h1>
+     <div className="flex shadow rounded-lg overflow-hidden my-5">
+      <input type="text" value={password} className='outline-none w-full py-1 px-3' placeholder="Password" readOnly ref={passwordRef}/>
+      <button className='outline-none bg-blue-700 text-white px-3 py-o.5 shrink-0' onClick={copyToClipBoard}>Copy</button>
      </div>
-     <div className="flex text-sm gap-x-2">
+     <div className="flex text-sm gap-x-2 my-3">
       <div className="flex items-center gap-x-1">
-        <input type="range" min={8} max={20} value={length} className='cursor-pointer' onChange={(e) => {setLength(e.target.value)}}/>
+        <input type="range" min={8} max={50} value={length} className='cursor-pointer' onChange={(e) => {setLength(e.target.value)}}/>
         <label>Length: {length}</label>
       </div>
       <div className="flex items-center gap-x-1">
