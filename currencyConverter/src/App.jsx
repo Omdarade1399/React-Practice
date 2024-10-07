95f4d151472b075da9170a3d1155ea758544381f
Omdarade1399 <150209165+Omdarade1399@users.noreply.github.com>
2024-10-07 21:26:38 +0530

    Built a Currency Converter App with React

--- currencyConverter/src/App.jsx
+++ currencyConverter/src/App.jsx
@@ -4,8 +4,8 @@ import useCurrencyInfo from './hooks/useCurrencyInfo'
 
 function App() {
   const [amount, setAmount] = useState(0)
-  const [from, setFrom] = useState("usd")
-  const [to, setTo] = useState("inr")
+  const [from, setFrom] = useState("inr")
+  const [to, setTo] = useState("usd")
   const [convertedAmount, setConvertedAmount] = useState("")
   
   const currencyInfo = useCurrencyInfo(from)
@@ -25,12 +25,12 @@ function App() {
   
   return (
     <>
-      <div className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat" style={{backgroundImage: `url("https://t4.ftcdn.net/jpg/01/17/49/87/360_F_117498748_uWmutwTMHBbeV0pO37jeggD3p3SelZhV.jpg")`}}>
+      <div className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat" style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)),url(https://t4.ftcdn.net/jpg/01/17/49/87/360_F_117498748_uWmutwTMHBbeV0pO37jeggD3p3SelZhV.jpg)`}}>
       <div className="w-full">
-        <div className="w-full max-w-md mx-auto border-2 border-grey-60 rounded-lg p-5 backdrop-blur-md bg-white-30">
+        <div className="w-full max-w-md mx-auto border border-grey-60 rounded-lg p-5 backdrop-blur-sm bg-white-30">
           <form onSubmit={(e) => {e.preventDefault(); convert()}} >
             <div className="w-full mb-1">
-              <InputBox label="From" amount={amount} currencyOptions={options} onCurrencyChange={(currecy) => setAmount(amount)} selectCurrency={from} onAmountChange={(amount) => setAmount(amount)} />
+              <InputBox label="From" amount={amount} currencyOptions={options} onCurrencyChange={(currency) => setFrom(currency)} selectCurrency={from} onAmountChange={(amount) => setAmount(amount)} />
             </div>
             <div className="relative w-full h-0 5">
               <button type='button' className='absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5' onClick={swap}>
@@ -38,7 +38,7 @@ function App() {
               </button>
             </div>
             <div className="w-full mt-1 mb-4">
-            <InputBox label="To" amount={convertedAmount} currencyOptions={options} onCurrencyChange={(currecy) => setTo(currecy)} selectCurrency={from} amountDisable />
+            <InputBox label="To" amount={convertedAmount} currencyOptions={options} onCurrencyChange={(currency) => setTo(currency)} selectCurrency={to} onAmountChange={(amount) => setAmount(amount)} amountDisable />
             </div>
             <button type="submit" className='w-full bg-blue-600 text-white px-4 py-3 rounded-lg'>Convert {from.toUpperCase()} to {to.toUpperCase()}</button>
           </form>
