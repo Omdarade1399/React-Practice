b3e42948da40040c51ac2a4038bd5809bad21a48
Omdarade1399 <150209165+Omdarade1399@users.noreply.github.com>
2024-10-17 16:51:21 +0530

    Learning useContext

--- miniContext/src/Components/Login.jsx
+++ miniContext/src/Components/Login.jsx
@@ -17,7 +17,11 @@ function Login() {
     <div>
         <h2>Login</h2>
         <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder='Username' />
+        <br />
+        <br />
         <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Password' />
+        <br />
+        <br />
         <button onClick={handleSubmit}>Submit</button>
     </div>
   )
